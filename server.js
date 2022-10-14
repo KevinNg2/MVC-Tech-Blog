7c4180bb67e01dafc9e81c0406c33426cd32756b
Kevin Ng <102876076+KevinNg2@users.noreply.github.com>
2022-10-14 08:52:56 -0700

    calling on live server

---
+++ server.js
@@ -0,0 +1,48 @@
+const path = require('path');
+const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const routes = require('./controllers');
+const helpers = require('./utils/helpers');
+
+const sequelize = require('./config/connection');
+
+
+const hbs = exphbs.create({ helpers });
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// create a new sequelize store using the express session package
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+// configure and link a session objet with the sequelize store
+const sess = {
+  secret: 'smallgreenturtle',
+  cookie: {
+        // Session will automatically expire in 10 minutes
+        expires: 10 * 60 * 1000
+  },
+  resave: true,
+  rolling: true,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  }),
+};
+// add express-session and store as express.js middleware
+app.use(session(sess));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(routes);
+
+// turn on connection to db and server
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
+});
